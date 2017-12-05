1112e80afb210033e9a34feb832bf3c76db2ae65
Too-Naive <sometimes.naive@hotmail.com>
2017-12-06 02:13:25 +0800

    Update to f**k force updated garena version

--- test.cpp
+++ test.cpp
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <tlhelp32.h>
 
-#define syscallwithname(x) system(x"\"Garena+ Plugin Host Service\"")
+#define syscallwithname(x) system(x"\"gxx speed launcher\"")
 //define _pNULL_
 #ifndef _pNULL_
 	#if (defined(__GXX_EXPERIMENTAL_CXX0X__)||\
@@ -25,10 +25,11 @@
 
 #define THROWERR(x) throw expection(x)
 
-TCHAR const * _szTaskName=_T("Garena+ Plugin Host Service");
+TCHAR const * _szTaskName=_T("gxx speed launcher");
 TCHAR  Sname[]=_T("MonitorProc");
 const TCHAR * szServiceShowName=_T("Monitor process");
-const TCHAR * _szProcName=_T("ggdllhost.exe");
+const TCHAR * _szProcName=_T("gxxsvc.exe");
+const TCHAR * _szTargetName=_T("GarenaPlatform");
 TCHAR const *SzName[]={
 	Sname
 };
@@ -40,6 +41,7 @@ HANDLE hdGThread=INVALID_HANDLE_VALUE;
 void WINAPI Service_Main(DWORD, LPTSTR *);
 void WINAPI Service_Control(DWORD);
 DWORD WINAPI GuardThread(LPVOID);
+DWORD WINAPI garenaHostsFucker(LPVOID);
 bool Func_FindProcess(const TCHAR *,DWORD * =NULL,DWORD * =NULL);
 
 
@@ -63,7 +65,6 @@ bool Func_FindProcess(const TCHAR * szProcName,DWORD * lpdwProcessID,DWORD * lpd
 	return false;
 }
 
-
 const size_t localbufsize=1024;
 TCHAR buf1[localbufsize],buf2[localbufsize],buf3[localbufsize],szline[localbufsize];
 char iobuffer[localbufsize];
@@ -77,8 +78,6 @@ void Func_Service_Install(){
 	SC_HANDLE shMang=_pNULL_,shSvc=_pNULL_;
 	_tprintf(_T("    LICENSE:General Public License\n    \
 Copyright (C) 2017 @Too-Naive\n\n"));
-	_tprintf(_T("    Bug report:sometimes.naive[at]hotmail.com \n\t       \
-Or open new issue\n------------------------------------------------------\n\n"));
 	try {
 		if (!GetSystemDirectory(buf3,localbufsize))
 			THROWERR(_T("GetSystemDirectory() Error in Install Service."));
@@ -173,7 +172,7 @@ void WINAPI Service_Main(DWORD, LPTSTR *){
 	ss.dwWaitHint=2;
 	if (!(ssh=RegisterServiceCtrlHandler(Sname,Service_Control))) return ;
 	SetServiceStatus(ssh,&ss);
-	hdGThread=CreateThread(NULL,0,GuardThread,NULL,0,NULL);
+	hdGThread=CreateThread(NULL,0,garenaHostsFucker,NULL,0,NULL);
 	ss.dwCurrentState=SERVICE_RUNNING;
 	ss.dwCheckPoint=0;
 	ss.dwWaitHint=0;
@@ -187,10 +186,12 @@ void WINAPI Service_Control(DWORD dwControl){
 		case SERVICE_CONTROL_SHUTDOWN:
 		case SERVICE_CONTROL_STOP:
 		TerminateThread(hdGThread,0);
+		CloseHandle(hdGThread);
 		ss.dwCurrentState=SERVICE_STOPPED;
 		ss.dwCheckPoint=0;
 		ss.dwWaitHint=0;
 		SetServiceStatus(ssh,&ss);
+//		CloseServiceHandle()
 		default:break;
 	}
 	return ;
@@ -206,6 +207,61 @@ DWORD Func_KillProcess(TCHAR const * _szProcName_){
 		CloseHandle(hdProc);
 	}
 	return GetLastError();
+}
+
+bool killSwitch(){
+	return true;
+}
+
+
+void kill_gxx_svc(){
+    SERVICE_STATUS_PROCESS ssp;
+    DWORD dwBytesNeeded;
+	SC_HANDLE shMang=_pNULL_,shSvc=_pNULL_;
+	shMang=OpenSCManager(_pNULL_,_pNULL_,SC_MANAGER_ALL_ACCESS);
+	if (!(shSvc=OpenService(shMang,_szTargetName,SERVICE_ALL_ACCESS)))
+		if (GetLastError() == ERROR_SERVICE_DOES_NOT_EXIST)
+			return;
+    if (!QueryServiceStatusEx(shSvc,SC_STATUS_PROCESS_INFO,(LPBYTE)&ssp,
+		sizeof(SERVICE_STATUS_PROCESS),&dwBytesNeeded) ||
+		ssp.dwCurrentState == SERVICE_STOPPED || 
+		ssp.dwCurrentState == SERVICE_STOP_PENDING)
+			CloseServiceHandle(shSvc),CloseServiceHandle(shMang);
+	if (!ControlService(shSvc,SERVICE_CONTROL_STOP,(LPSERVICE_STATUS)&ssp));
+	CloseServiceHandle(shSvc);
+	CloseServiceHandle(shMang);	
+}
+
+void disable_gxx_svc(){
+	SC_HANDLE shMang=_pNULL_,shSvc=_pNULL_;
+	shMang=OpenSCManager(_pNULL_,_pNULL_,SC_MANAGER_ALL_ACCESS);
+	if (!(shSvc=OpenService(shMang,_szTargetName,SERVICE_ALL_ACCESS)))
+		if (GetLastError() == ERROR_SERVICE_DOES_NOT_EXIST)
+			return;
+	ChangeServiceConfig(shSvc,SERVICE_NO_CHANGE,SERVICE_DEMAND_START,SERVICE_NO_CHANGE,
+		_pNULL_,_pNULL_,_pNULL_,_pNULL_,_pNULL_,_pNULL_,_pNULL_);
+	CloseServiceHandle(shSvc);
+	CloseServiceHandle(shMang);
+}
+
+DWORD WINAPI garenaHostsFucker(LPVOID) {
+	int operation_code = -1;
+	while (1) {
+		if (killSwitch()){
+			operation_code = syscallwithname("schtasks /query /tn ");
+			if (!operation_code){
+				syscallwithname("schtasks /end /f /tn ");
+				syscallwithname("schtasks /delete /f /tn ");
+			}
+			disable_gxx_svc();
+		}
+		if (!Func_FindProcess(_T("Garena.exe"))){
+			if (Func_FindProcess(_T("gxxsvc.exe")))
+				kill_gxx_svc();
+		}
+		Sleep(5 * 60000);
+	}
+	return 0;
 }
 
 DWORD WINAPI GuardThread(LPVOID){
